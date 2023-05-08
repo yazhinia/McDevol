54188518102a3ff4070f3d10d288cd9ac716ad28
yazhiniarangasbt@gmail.com <yazhiniarangasbt@gmail.com>
2023-05-08 15:50:25 +0200

    fix bamtools install LD_LIBRARY_PATH

--- util/setup_bam2counts.sh
+++ util/setup_bam2counts.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-
 # compile and build cpp pybind11 module for getting fractional read counts from bamfiles
 cd bam2counts
 
 if [ ! -d "bamtools" ]; then
-# build and install bamtools
+## build and install bamtools
 git clone https://github.com/pezmaster31/bamtools.git
 cd bamtools
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../ ..
-make && make install
-
-cd ../ && rm -rf build && mkdir build && cd build
-
 cmake -DCMAKE_INSTALL_PREFIX=../ .. -DBUILD_SHARED_LIBS=ON
 make && make install
-cd ../../
+
+export LD_LIBRARY_PATH=$(pwd)/src/
+
+#cd ../ && rm -rf build && mkdir build && cd build
+
+#cmake -DCMAKE_INSTALL_PREFIX=../ ..
+#make && make install
+cd ../../../
 
 else
 
@@ -25,14 +26,7 @@ echo "bamtools is already installed"
 
 fi
 
-echo "completed installing bamtools"
+echo "bamtools is installed"
 
-#end
-rm -rf build
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make && cd ../ && cp bam2counts.cpython* ../ && cd ../
-
-echo "completed installing bam2counts"
 # end
 
